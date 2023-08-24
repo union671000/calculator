75de0d49b183a15da387ca367fadf088444544e7
union671000 <union671000@naver.com>
2023-08-24 10:50:09 +0900

    Add Spring Boot skeleton

---
+++ src/test/java/com/union671000/calculator/CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.union671000.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
