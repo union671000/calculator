bb65921b65e70062441c21d4334d391c09f38796
union671000 <union671000@naver.com>
2023-08-25 10:57:00 +0900

    Add sum logic, controller and unit test

---
+++ src/main/java/com/union671000/calculator/Calculator.java
@@ -0,0 +1,11 @@
+package com.union671000.calculator;
+
+import org.springframework.stereotype.Service;
+
+@Service
+
+public class Calculator {
+	public int sum(int a, int b) {
+		return a + b;
+	}
+}
---
+++ src/main/java/com/union671000/calculator/CalculatorTest.java
@@ -0,0 +1,12 @@
+package com.union671000.calculator; 
+import org.junit.Test; 
+import static org.junit.Assert.assertEquals; 
+ 
+public class CalculatorTest { 
+     private Calculator calculator = new Calculator(); 
+ 
+     @Test 
+     public void testSum() { 
+          assertEquals(5, calculator.sum(2, 3)); 
+     } 
+} 
